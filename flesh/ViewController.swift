a0c21166040ce2933b7d05ac6d499157a38879ab
ethanxiang <674712964@qq.com>
2019-06-10 23:24:17 +0800

    initialize and set app icon

---
+++ flesh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flesh
+//
+//  Created by Ethan Xiang on 2019/6/10.
+//  Copyright © 2019 Ethan Xiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
