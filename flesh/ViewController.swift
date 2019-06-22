60efb03b0616edbb1c28d72d5fe512d883750642
ethanxiang <674712964@qq.com>
2019-06-22 13:33:43 +0800

    support to immersive status bar

--- flesh/ViewController.swift
+++ flesh/ViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    
+    @IBOutlet weak var navigationBarTitle: UINavigationItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // set navigationBar color
+        navigationBar.backgroundColor = UIColor.colorWithHex(hexStr:"#EA2669")
+        navigationBar.barTintColor = UIColor.colorWithHex(hexStr:"#EA2669")
+        
+        // initialize navigationBar
+        navigationBarTitle.title = Bundle.main.infoDictionary!["CFBundleDisplayName"] as? String //App 名称
+        navigationBarTitle.backBarButtonItem = UIBarButtonItem(title: "返回", style: UIBarButtonItem.Style.plain, target: nil, action: nil)
+        navigationBarTitle.leftBarButtonItem = UIBarButtonItem(title: "返回", style: UIBarButtonItem.Style.plain, target: nil, action: nil)
+        
     }
-
+    
 }
+
+
 
